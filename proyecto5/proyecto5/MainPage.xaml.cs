728de98401ed87cfd5efd727b3703c2dd08578f8
Jose-Pater294 <jose.paterninas@cecar.edu.co>
2024-10-18 08:43:12 -0500

    Añadi nuev pagina llamada Pater

--- proyecto5/proyecto5/MainPage.xaml.cs
+++ proyecto5/proyecto5/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace proyecto5
+﻿using TuAplicacion;
+
+namespace proyecto5
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +11,9 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Navigation.PushAsync(new Pater());
         }
     }
 
---
+++ proyecto5/proyecto5/Pater.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+
+namespace TuAplicacion
+{
+    public partial class Pater : ContentPage
+    {
+        public Pater()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
