66496593452d34ef5f6b7b936c2037907a885863
LAPTOP-A0MQDKN7\57311 <jesus.villalbav@cecar.edu.co>
2024-10-18 08:17:17 -0500

    primer commit a main

---
+++ proyecto5/proyecto5/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace proyecto5
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ proyecto5/proyecto5/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace proyecto5
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
